25ebc11596415566376695a811d51e5c1ff07c63
Parth Nikam <parthsnikam12co@gmail.com>
2023-08-29 23:47:39 +0530

    Operating Turtlebot3 using teleop

--- src/scripts/Config.js
+++ src/scripts/Config.js
@@ -2,7 +2,7 @@ const Config = {
     ROSBRIDGE_SERVER_IP: "192.168.64.10",
     ROSBRIDGE_SERVER_PORT: "9090",
     RECONNECTION_TIME: "2000",
-    CMD_VEL_TOPIC: "/turtle1/cmd_vel"
+    CMD_VEL_TOPIC: "/cmd_vel"
 }
 
 export default Config
