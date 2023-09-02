910a909ce01af0b2c769916e64d57eb371a82896
Parth Nikam <parthsnikam12co@gmail.com>
2023-09-02 11:17:05 +0530

    Publishing Pose and Velocity on the website

--- src/scripts/Config.js
+++ src/scripts/Config.js
@@ -2,7 +2,9 @@ const Config = {
     ROSBRIDGE_SERVER_IP: "192.168.64.10",
     ROSBRIDGE_SERVER_PORT: "9090",
     RECONNECTION_TIME: "2000",
-    CMD_VEL_TOPIC: "/cmd_vel"
+    CMD_VEL_TOPIC: "/cmd_vel",
+    ODOM_TOPIC: "/odom",
+    POSE_TOPIC: "/amcl_pose"
 }
 
 export default Config
